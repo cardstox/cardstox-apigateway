850cebbe128a57a16bac0e1c0414c58e3aec568e
Jim Belcher <jbelcher1985@yahoo.com>
2018-01-15 13:17:00 -0500

    added cards routing

--- src/main/java/com/cardstox/apigateway/ApigatewayApplication.java
+++ src/main/java/com/cardstox/apigateway/ApigatewayApplication.java
@@ -2,8 +2,10 @@ package com.cardstox.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableZuulProxy
 public class ApigatewayApplication {
